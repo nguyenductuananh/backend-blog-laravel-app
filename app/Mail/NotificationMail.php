ef3ae010b7c4ac415fc7cf38bc35c0c876de78bc
nguyenductuananh <nguyenductuananh19@gmail.com>
2022-09-16 21:58:50 +0700

    update base service

---
+++ app/Mail/NotificationMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('thank', ['body' => "You receive a notification, let's go to web and check it now !!!!", "title" => "You received a notification"]);
+    }
+}
